6828b87165ea3af9a00e92c749c56c402a60f4ef
ArmandoSudi <armando.sudi@gmail.com>
2017-10-24 09:08:02 +0100

    add the serializers of all the categories

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, AutoPost, FurniturePost, ElectronicPost, HousePost, LawnPost, JobAndServicePost
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+
+class AutoPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AutoPost
+        fields = '__all__'
+
+class FurniturePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FurniturePost
+        fields = '__all__'
+
+class ElectronicPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ElectronicPost
+        fields = '__all__'
+
+class HousePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HousePost
+        fields = '__all__'
+
+class LawnPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LawnPost
+        fields = '__all__'
+
+class JobAndServicePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobAndServicePost
         fields = '__all__'
