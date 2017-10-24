efea08e59251337333ac87ae109ee244e54ad782
ArmandoSudi <armando.sudi@gmail.com>
2017-10-24 09:10:45 +0100

    add the urls for all the categories

--- backend/urls.py
+++ backend/urls.py
@@ -9,6 +9,31 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetail.as_view()),
     url(r'^update_post/(?P<pk>[0-9]+)/$', views.PostUpdate.as_view()),
     url(r'^update/(?P<pk>[0-9]+)/$$', views.update_post),
+
+    # auto post urls
+    url(r'^auto_posts/$', views.AutoPostList.as_view()),
+    url(r'^auto_post/(?P<pk>[0-9]+)/$', views.AutoPostDetail.as_view()),
+
+    # furniture post urls
+    url(r'^furniture_posts/$', views.FurniturePostList.as_view()),
+    url(r'^furniture_post/(?P<pk>[0-9]+)/$', views.FurniturePostDetail.as_view()),
+
+    # electronic post urls
+    url(r'^electronic_posts/$', views.ElectronicPostList.as_view()),
+    url(r'^electronic_post/(?P<pk>[0-9]+)/$', views.ElectronicPostDetail.as_view()),
+
+    #house post urls
+    url(r'^house_posts/$', views.HousePostList.as_view()),
+    url(r'^house_post/(?P<pk>[0-9]+)/$', views.HousePostDetail.as_view()),
+
+    # lawn post urls
+    url(r'^lawn_posts/$', views.LawnPostList.as_view()),
+    url(r'^lawn_post/(?P<pk>[0-9]+)/$', views.LawnPostDetail.as_view()),
+
+    # job and service post urls
+    url(r'^job_and_service_posts/$', views.JobAndServicePostList.as_view()),
+    url(r'^job_and_servicep_post/(?P<pk>[0-9]+)/$', views.JobAndServicePostDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
