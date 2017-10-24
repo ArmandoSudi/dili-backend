91f05ac5ba05ff48917d511d48fd61ce5add01a1
ArmandoSudi <armando.sudi@gmail.com>
2017-10-24 09:08:45 +0100

    add the admin model for all the categories

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, AutoPost, FurniturePost, ElectronicPost, HousePost, LawnPost, JobAndServicePost
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
 
+class AutoPostAdmin(admin.ModelAdmin):
+    model = AutoPost
+
+class FurniturePostAdmin(admin.ModelAdmin):
+    model = FurniturePost
+
+class ElectronicPostAdmin(admin.ModelAdmin):
+    model = ElectronicPost
+
+class HousePostAdmin(admin.ModelAdmin):
+    model = HousePost
+
+class HousePostAdmin(admin.ModelAdmin):
+    model = HousePost
+
+class LawnPostAdmin(admin.ModelAdmin):
+    model = LawnPost
+
+class JobAndServicePostAdmin(admin.ModelAdmin):
+    model = JobAndServicePost
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(AutoPost, AutoPostAdmin)
+admin.site.register(FurniturePost, FurniturePostAdmin)
+admin.site.register(ElectronicPost, ElectronicPostAdmin)
+admin.site.register(HousePost, HousePostAdmin)
+admin.site.register(LawnPost, LawnPostAdmin)
+admin.site.register(JobAndServicePost, JobAndServicePostAdmin)
